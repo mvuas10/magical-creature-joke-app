05ec568af0cafb4f08d6c91ce3f0aa2134b0c3ce
mvuas10 <mvuas10@gmail.com>
2020-04-23 15:29:27 +0200

    Define route for jokes

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Listening on: ${port}`));
+app.listen(port, () => {
+  console.log(`Listening on: ${port}`);
+});
+
+app.get("/jokes", () => {
+  console.log("Request...");
+});
