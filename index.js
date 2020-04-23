ce247e8443abaf38464e437a2fab7d95d8dda3f3
mvuas10 <mvuas10@gmail.com>
2020-04-23 15:41:14 +0200

    Trial 1 - test with object joke

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ app.listen(port, () => {
   console.log(`Listening on: ${port}`);
 });
 
-app.get("/jokes", (request, response) => {
+const joke = {
+  joke_1: "What do you call a boommeran that doesn't work? A stick",
+  joke_2:
+    "The same bike tries to run me down every day. Sounds like a vicious cycle",
+  joke_3: "What do you call a guy who's had too much to drink? A cab",
+  joke_4: "What kinds of tree has a hand? A palm tree",
+  joke_5: "How does the solor system organize a party? They planet!",
+};
+
+app.get("/jokes/:age", (request, response) => {
+  const age = parseInt(request.params.age);
+
   console.log("Request...");
-  response.send("Hello World!");
+  //If age is <= 30 then tell joke 1
+  //If age is > 30 then tell joke 2
+  if (age <= 30) {
+    response.send(joke.joke_1);
+  } else {
+    response.send("Dude you are old");
+  }
 });
